c5cf72803baa8f95d164310356952bff91468e21
kevincheng <kevincheng401@gmail.com>
2023-10-17 20:56:05 +0800

    Finish AList

--- lec7_lists4/DIY/AList.java
+++ lec7_lists4/DIY/AList.java
@@ -4,32 +4,61 @@ package lec7_lists4.DIY;
  *  in case you want to try to figure out how to write it yourself.
  *  After writing your methods, you can run the AListTest file.
  */
-public class AList {
+public class AList<Item> {
+
+/* invariants (things that must be true):
+ addLast: The next Item we want to add, will go into position size.
+ getLast: The item we want to return is in position size - 1.
+ size: The number of items in the list should be size.
+ */
+    private Item[] items;
+    private int size;
     /** Creates an empty list. */
     public AList() {
+        items = (Item[]) new Object[8];
+        size = 0;
+    }
+
+    /*
+     Define the "usage ratio" R = size / items.length;
+     Typical solution: Half array size when R < 0.25.
+     */
+    private void resize(int capacity){
+        Item[] a = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
     }
 
     /** Inserts X into the back of the list. */
-    public void addLast(int x) {
+    public void addLast(Item x) {
+        if (size == items.length){
+//          resize the array by multiply
+            resize(size * 2);
+        }
+        items[size] = x;
+        size += 1;
     }
 
     /** Returns the item from the back of the list. */
-    public int getLast() {
-        return 0;        
+    public Item getLast() {
+        return items[size - 1];
     }
     /** Gets the ith item in the list (0 is the front). */
-    public int get(int i) {
-        return 0;        
+    public Item get(int i) {
+        return items[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
-    public int removeLast() {
-        return 0;
+    public Item removeLast() {
+        Item returnItem = getLast();
+        items[size - 1] = null; // Prevent from loitering
+        size -= 1;
+        return returnItem;
     }
 } 
