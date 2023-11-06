5408a06ac751813023aa96fe8645753bb9bc05cb
kevincheng <kevincheng401@gmail.com>
2023-11-06 18:12:20 +0800

    Complete comparator part

--- lec11_inheritance3/Dog.java
+++ lec11_inheritance3/Dog.java
@@ -1,24 +1,33 @@
 package lec11_inheritance3;
 
-public class Dog implements Comparable<Dog> {
+import java.util.Comparator;
+
+public class Dog implements Comparable<Dog>{
     public String name;
     private int size;
-
-    public Dog(String n, int s) {
+    public Dog(String n, int s){
         name = n;
         size = s;
     }
 
     @Override
-    public int compareTo(Dog uddaDog) {
-        //assume nobody is messing up and giving us
-        //something that isn't a dog.
-        return size - uddaDog.size;
+    public int compareTo(Dog o) {
+        return size - o.size;
     }
 
+    private static class NameComparator implements Comparator<Dog>{
+        @Override
+        public int compare(Dog a, Dog b) {
+            return a.name.compareTo(b.name); // The compareTo() is String.compareTo(), which compares two strings lexicographically.
+        }
+    }
 
+    public static Comparator<Dog> getNameComparator(){
+        return new NameComparator();
+    }
 
     public void bark() {
         System.out.println(name + " says: bark");
     }
+
 }
